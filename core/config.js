825e46ef1279833b3d1f51176ecde5eb729fc452
Sokhibjon Orzikulov <genemators@gmail.com>
2020-07-19 04:48:28 +0500

    Domain dammit

--- core/config.js
+++ core/config.js
@@ -2,7 +2,7 @@ const token = process.env.BOT_TOKEN
 const owner = process.env.OWNER | 756870298
 const environment = process.env.ENVIRONMENT
 const minecraft = process.env.MINECRAFT
-const domain = process.env.URL
+const domain = process.env.DOMAIN
 const port = process.env.PORT
 
 module.exports = {
