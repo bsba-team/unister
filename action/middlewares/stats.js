f17913ea828930a7a56c14d7d441c8738555644a
Sokhibjon Orzikulov <genemators@gmail.com>
2020-07-30 19:43:41 -0400

    Stats indicators upgraded

--- action/middlewares/stats.js
+++ action/middlewares/stats.js
@@ -6,8 +6,16 @@ const database = require('../../database/db')
 
 composer.command(`stats`, async ctx => {
     const status = {
+        id: ctx.from.id,
         first_name: ctx.from.first_name,
         last_name: ctx.from.last_name,
+        username: ctx.from.username,
+        captcha: async () => {
+            if (ctx.from.is_bot)
+                return `bot`
+            else
+                return `person`
+        },
         lang: ctx.from.language_code,
         superuser: async () => {
             if ( database.users["eternal"].includes(ctx.from.id) || database.users["temporary"].includes(ctx.from.username)) {
@@ -21,8 +29,11 @@ composer.command(`stats`, async ctx => {
     await ctx.replyWithHTML(
         `<b>User status preview:</b>` + `\n` +
         `\n` +
+        `<b>ID:</b> <code>${status.id}</code>` + `\n` +
         `<b>First Name:</b> <code>${status.first_name}</code>` + `\n` +
         `<b>Last Name:</b> <code>${status.last_name}</code>` + `\n` +
+        `<b>Username:</b> <code>${status.username}</code>` + `\n` +
+        `<b>Captcha:</b> <code>${await status.captcha()}</code>` + `\n` +
         `<b>Language:</b> <code>${status.lang}</code>` + `\n` +
         `<b>Status:</b> <code>${await status.superuser()}</code>`
     )
