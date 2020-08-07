0b04859be2f2074b8ee3c200a7e4eedbd7b15a3d
Sokhibjon Orzikulov <genemators@gmail.com>
2020-08-06 21:01:20 -0400

    Error and Security Tab redesigned

--- action/exclude/index.js
+++ action/exclude/index.js
@@ -7,7 +7,9 @@ const keyboard = require('../../layouts/keyboards')
 composer.on('text', async ctx => {
     if (ctx.chat.type === "private")
         if (!ctx.message["via_bot"])
-            await ctx.replyWithHTML(message.invalid, {
+            await ctx.replyWithAnimation({url: `https://media.giphy.com/media/xT77XGWy4IRCeRbWg0/source.gif`}, {
+                parse_mode: "HTML",
+                caption: message.invalid,
                 reply_markup: keyboard.invalid
             })
 })
--- action/security/index.js
+++ action/security/index.js
@@ -6,7 +6,9 @@ module.exports = async (ctx, func) => {
     if ( database.users["eternal"].includes(ctx.from.id) || database.users["temporary"].includes(ctx.from.username)) {
         await func()
     } else {
-        await ctx.replyWithHTML(message.error_admin, {
+        await ctx.replyWithAnimation({url: `https://media.giphy.com/media/xT77XUjQrAVQmf4jFS/source.gif`}, {
+            parse_mode: "HTML",
+            caption: message.error_admin,
             reply_markup: keyboard.error_admin
         })
     }
